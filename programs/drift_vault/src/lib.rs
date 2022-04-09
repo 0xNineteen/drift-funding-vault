fa3eced5993abae226695805fc9adbcd3ee4b65a
0xNineteen <0x39015319@gmail.com>
2022-04-09 12:17:11 -0400

    vault withdraw working

--- programs/drift_vault/src/lib.rs
+++ programs/drift_vault/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_spl::{
     associated_token::AssociatedToken,
-    token::{Mint, Token, TokenAccount, mint_to, MintTo, Transfer, transfer},
+    token::{Mint, Token, TokenAccount, mint_to, MintTo, Transfer, transfer, Burn, burn},
 };
 
 use clearing_house::context::{
@@ -34,8 +34,6 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod drift_vault {
-
-    use clearing_house::math::collateral;
 
     use super::*;
     pub fn initialize_vault(
@@ -128,7 +126,8 @@ pub mod drift_vault {
         )?;
 
         // deposit usdc to vault's drift collateral 
-        // 2 step process: [depositer => {vault collateral] => drift account} :(
+        // 2 step process bc of auth: 
+        // [depositer => {vault collateral] => drift account}
 
         // (1) depositer => vault collateral 
         transfer(CpiContext::new(
@@ -180,7 +179,144 @@ pub mod drift_vault {
     // close position size (relative to amount of burn_pool_tokens)
     // transfer from vault => user 
     // emit widthdraw event 
-    // * update position ()
+    // * update position () 
+    pub fn withdraw(
+        ctx: Context<Withdraw>, 
+        burn_amount: u128,
+        market_index: u64,
+        authority_nonce: u8,
+    ) -> ProgramResult {
+        let update_position_accounts = &mut ctx.accounts.update_position;
+
+        // compute total amount of collateral 
+        let collateral_amount = update_position_accounts.user.collateral;
+        let liabilites_amount = update_position_accounts.compute_total_liabilies();
+        msg!("(collateral, liabilities) amount: {}, {}", collateral_amount, liabilites_amount);
+
+        let user_vault_balance = ctx.accounts.user_vault_ata.amount as u128; 
+        require!(user_vault_balance >= burn_amount, VaultErrorCode::NotEnoughFunds);
+
+        let state = &mut ctx.accounts.vault_state; 
+        // collateral to give = (burn_amount / total_minted) * total_colateral
+        let mut refund_collateral_amount = 
+            burn_amount
+                .checked_mul(collateral_amount).unwrap()
+                .checked_div(state.total_amount_minted as u128).unwrap() as u64;
+        msg!("estimated refund amount: {}", refund_collateral_amount);
+        require!(refund_collateral_amount > 0, VaultErrorCode::WidthdrawAmountTooSmall);
+
+        let authority_seeds = [
+            b"authority".as_ref(),
+            &[authority_nonce][..],
+        ];
+        let signers = &[&authority_seeds[..]];
+
+        // reduce position (want approx 1:1 collat:liabilities)
+        let new_collateral_amount = collateral_amount - refund_collateral_amount as u128;
+        let amount_to_reduce = liabilites_amount - new_collateral_amount; 
+        let vault_position = update_position_accounts.get_current_position(market_index);
+        msg!("vaults current position: {:?}", vault_position);
+
+        if vault_position != Position::None {
+            let reduce_direction = match vault_position {
+                Position::Long => {
+                    msg!("position reducing: {:?} {:?}", Position::Short, amount_to_reduce);
+                    ClearingHousePositionDirection::Short
+                },
+                Position::Short => {
+                    msg!("position reducing: {:?} {:?}", Position::Long, amount_to_reduce);
+                    ClearingHousePositionDirection::Long
+                },
+                _ => panic!("shouldnt be called...")
+            };
+        
+            update_position_accounts.open_position(
+                amount_to_reduce, 
+                0, 
+                reduce_direction, 
+                signers,
+                market_index,
+            )?;
+
+            // compute total amount of collateral 
+            update_position_accounts.user.reload()?;
+            let collateral_amount = update_position_accounts.user.collateral;
+            let liabilites_amount = update_position_accounts.compute_total_liabilies();
+            msg!("(collateral, liabilities) amount: {}, {}", collateral_amount, liabilites_amount);        
+
+            // re-compute refund amount after close (collateral estimate isnt perfect bc slippage + fees)
+            refund_collateral_amount = 
+                burn_amount
+                .checked_mul(collateral_amount).unwrap()
+                .checked_div(state.total_amount_minted as u128).unwrap() as u64;
+            msg!("estimated refund amount: {}", refund_collateral_amount);
+            require!(refund_collateral_amount > 0, VaultErrorCode::WidthdrawAmountTooSmall);
+        }
+
+        // withdraw + transfer to user 
+        // (1) drift => vault collateral
+        let cpi_program = update_position_accounts.clearing_house_program.to_account_info();
+        let cpi_accounts = ClearingHouseWithdrawCollateral {
+            // user stuff 
+            user: update_position_accounts.user.to_account_info(), // PDA
+            user_collateral_account: ctx.accounts.vault_collateral_ata.to_account_info(), // [!]
+            user_positions: update_position_accounts.user_positions.to_account_info(),// KP
+            authority: update_position_accounts.authority.clone(), // KP 
+
+            // drift stuff 
+            state: update_position_accounts.state.to_account_info(), // CH 
+            markets: update_position_accounts.markets.to_account_info(), // CH 
+            collateral_vault: ctx.accounts.collateral_vault.to_account_info(), // CH 
+            deposit_history: ctx.accounts.deposit_history.to_account_info(),// CH 
+            funding_payment_history: update_position_accounts.funding_payment_history.to_account_info(), // CH 
+
+            collateral_vault_authority: ctx.accounts.collateral_vault_authority.to_account_info(),// CH 
+            insurance_vault: ctx.accounts.insurance_vault.to_account_info(),// CH 
+            insurance_vault_authority: ctx.accounts.insurance_vault_authority.to_account_info(),// CH 
+            
+            // other
+            token_program: ctx.accounts.token_program.to_account_info(), // basic
+        };
+        let cpi_ctx = CpiContext::new_with_signer(
+            cpi_program, 
+            cpi_accounts,
+            signers
+        );
+        clearing_house::cpi::withdraw_collateral(cpi_ctx, refund_collateral_amount)?;
+
+        // sanity check (make sure we got the correct output)
+        let balance_before = ctx.accounts.vault_collateral_ata.amount;
+        ctx.accounts.vault_collateral_ata.reload()?; // update underlying 
+        let balance_after = ctx.accounts.vault_collateral_ata.amount;
+        require!(balance_after - balance_before == refund_collateral_amount, 
+            VaultErrorCode::WidthdrawAmountTooSmall); 
+
+        // (2) vault collateral => user ata 
+        transfer(CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            Transfer {
+                from: ctx.accounts.vault_collateral_ata.to_account_info(),
+                to: ctx.accounts.user_collateral_ata.to_account_info(),
+                authority: update_position_accounts.authority.to_account_info(),
+            }
+        ).with_signer(signers), refund_collateral_amount)?;
+
+        // burn pool tokens 
+        burn(CpiContext::new(
+            ctx.accounts.token_program.to_account_info(), 
+        Burn { 
+                mint: ctx.accounts.vault_mint.to_account_info(), 
+                to: ctx.accounts.user_vault_ata.to_account_info(), 
+                authority: ctx.accounts.owner.to_account_info(),
+            }
+        ), burn_amount as u64)?;
+        
+        // update state 
+        state.total_amount_minted = state.total_amount_minted
+            .checked_sub(burn_amount as u64).unwrap(); 
+
+        Ok(())
+    }
 
     // ** update position 
     // compute mark price (amm.base_amount ... )
@@ -211,25 +347,7 @@ pub mod drift_vault {
         }
         
         // get current position (LONG, SHORT, or NONE)
-        let vault_position = {
-            let vault_positions = &ctx.accounts.user_positions.load()?;
-            let position_index = vault_positions
-                .positions
-                .iter()
-                .position(|market_position| market_position.is_for(market_index));
-
-            match position_index {
-                None => Position::None, 
-                Some(position_index) => {
-                    let vault_market_position = &vault_positions.positions[position_index];
-                    if vault_market_position.base_asset_amount > 0 { 
-                        Position::Long
-                    } else { 
-                        Position::Short
-                    }
-                }   
-            }
-        };
+        let vault_position = ctx.accounts.get_current_position(market_index);
         msg!("current position: {:?}", vault_position);
         
         // get authority signature 
@@ -259,7 +377,7 @@ pub mod drift_vault {
         msg!("amount_to_trade: {}", amount_to_trade);
 
         /* For now, if we need to reverse we use 2 steps (close, new_pos) but 
-         * we can do this in a single step for less fees -- but we keep it simple for now 
+         * in future we can do this in a single step for less fees 
          */
 
         if approx_funding < 0 { // funding goes to longs 
@@ -459,6 +577,43 @@ pub struct Deposit<'info> {
 }
 
 #[derive(Accounts)]
+pub struct Withdraw<'info> {
+    #[account(signer)]
+    pub owner: AccountInfo<'info>, // depositer / owner of ATAs 
+
+    // atas 
+        // vault 
+    #[account(mut, constraint = &vault_collateral_ata.mint.eq(&update_position.state.collateral_mint))]
+    pub vault_collateral_ata: Box<Account<'info, TokenAccount>>,  
+        // user
+    #[account(mut, constraint = &user_collateral_ata.mint.eq(&update_position.state.collateral_mint))]
+    pub user_collateral_ata: Box<Account<'info, TokenAccount>>, 
+    #[account(mut, has_one = owner, constraint = &user_vault_ata.mint.eq(&vault_mint.key()))]
+    pub user_vault_ata: Box<Account<'info, TokenAccount>>,  
+
+    // vault stuff 
+    #[account(mut, seeds = [b"vault_state".as_ref()], bump)] 
+    pub vault_state: Account<'info, VaultState>,
+    #[account(mut, seeds = [b"vault_mint".as_ref()], bump)] 
+    pub vault_mint: Account<'info, Mint>,
+
+    // additional drift things 
+    #[account(mut)]
+    pub collateral_vault: Box<Account<'info, TokenAccount>>,
+    pub collateral_vault_authority: AccountInfo<'info>,
+    #[account(mut)]
+    pub deposit_history: AccountInfo<'info>,
+    #[account(mut)]
+    pub insurance_vault: Box<Account<'info, TokenAccount>>,
+    pub insurance_vault_authority: AccountInfo<'info>,
+
+    pub update_position: UpdatePosition<'info>, // lots of drift things 
+
+    // other
+    pub token_program: Program<'info, Token>,
+}
+
+#[derive(Accounts)]
 pub struct UpdatePosition<'info> {
     #[account(mut, seeds = [b"authority".as_ref()], bump)]
     pub authority: AccountInfo<'info>,
@@ -466,7 +621,7 @@ pub struct UpdatePosition<'info> {
     pub user_positions: AccountLoader<'info, UserPositions>,
 
     #[account(mut)]
-    pub state: AccountInfo<'info>,
+    pub state: Box<Account<'info, State>>,
     #[account(mut)]
     pub user: Box<Account<'info, User>>,
     #[account(mut)]
@@ -482,6 +637,29 @@ pub struct UpdatePosition<'info> {
 }
 
 impl<'info> UpdatePosition<'info> {
+
+    fn get_current_position(
+        &self, 
+        market_index: u64,
+    ) -> Position {
+        let vault_positions = &self.user_positions.load().unwrap();
+        let position_index = vault_positions
+            .positions
+            .iter()
+            .position(|market_position| market_position.is_for(market_index));
+
+        match position_index {
+            None => Position::None, 
+            Some(position_index) => {
+                let vault_market_position = &vault_positions.positions[position_index];
+                if vault_market_position.base_asset_amount > 0 { 
+                    Position::Long
+                } else { 
+                    Position::Short
+                }
+            }   
+        }
+    }
 
     fn compute_total_liabilies(
         &self
@@ -590,4 +768,12 @@ macro_rules! math_error {
             error_code
         }
     }};
+}
+
+#[error]
+pub enum VaultErrorCode {
+    #[msg("Not enough funds.")]
+    NotEnoughFunds,
+    #[msg("Widthdraw amount too small.")]
+    WidthdrawAmountTooSmall,
 }
--- tests/drift_vault.ts
+++ tests/drift_vault.ts
@@ -192,13 +192,14 @@ describe('drift_vault', () => {
     return new BN(vault_balance.value.amount)
   }
 
+  let user_vault_ata; 
   it('deposits into vault', async () => {
 
     const _depositAmount = 1_000; 
     const depositAmount = new BN(_depositAmount * 10 ** 6);
 
     // create ata of vault mint 
-    let user_vault_ata = await token.Token.getAssociatedTokenAddress(
+    user_vault_ata = await token.Token.getAssociatedTokenAddress(
       token.ASSOCIATED_TOKEN_PROGRAM_ID, 
       token.TOKEN_PROGRAM_ID, 
       vault_mint, 
@@ -404,149 +405,63 @@ describe('drift_vault', () => {
 
   })
 
-  return; 
+  it('withdraws from the vault', async () => {
+    const market = clearingHouse.getMarket(marketIndex); 
+    const solUsd = market.amm.oracle;
 
-  it('does something drift', async () => {
-      // call initialize_vault instruction with a payer 
-      // user is already created 
-      // create a new open-orders drift account 
+    var user_vault_balance = await get_token_balance(user_vault_ata)
+    var user_usdc_balance = await get_token_balance(userUSDCAccount.publicKey)
 
-      const [userAccountPublicKey, userAccountPublicKeyNonce] = 
-        await drift.getUserAccountPublicKeyAndNonce(
-          CH_program.programId,
-          provider.wallet.publicKey,
-        );
-        
-      const optionalAccounts = {
-        whitelistToken: false,
-      };
+    let burn_amount = user_vault_balance; 
 
-      // create new drift account for provider.wallet
-      let userPositions = web3.Keypair.generate();
-      let ch_state_pk = await clearingHouse.getStatePublicKey(); 
-      await CH_program.rpc.initializeUserWithExplicitPayer(
-        userAccountPublicKeyNonce, 
-        optionalAccounts, 
-        {
-          accounts: {
-            user: userAccountPublicKey, // CH PDA
-            state: ch_state_pk, // CH 
-            userPositions: userPositions.publicKey, // PK 
-            authority: provider.wallet.publicKey, // PK 
-            payer: provider.wallet.publicKey, // PK 
-            rent: web3.SYSVAR_RENT_PUBKEY,
-            systemProgram: web3.SystemProgram.programId,
-          },
-          signers: [userPositions]
-        }
-      );
-
-      // deposit collateral 
-      const _depositAmount = 1_000; 
-      const depositAmount = new BN(_depositAmount * 10 ** 6);
-
-      const clearingHouseState = clearingHouse.getStateAccount();
-
-      // user amount => collateral account for provider.wallet
-      await CH_program.rpc.depositCollateral(depositAmount, {
+    let ix = vault_program.instruction.withdraw(
+      burn_amount, //.div(new BN(100)), // 1% widthdraw 
+      marketIndex, 
+      authority_b,
+      {
         accounts: {
-          user: userAccountPublicKey, 
-          userPositions: userPositions.publicKey,
-          authority: provider.wallet.publicKey,
-          userCollateralAccount: userUSDCAccount.publicKey, // ! 
-          // CH things 
-          state: ch_state_pk,
+          owner: provider.wallet.publicKey, 
+          userVaultAta: user_vault_ata, 
+          userCollateralAta: userUSDCAccount.publicKey,
+          vaultCollateralAta: vault_collateral,
+          
+          vaultMint: vault_mint, 
+          vaultState: vault_state, 
+          
           collateralVault: clearingHouseState.collateralVault,
-          markets: clearingHouseState.markets,
+          collateralVaultAuthority: clearingHouseState.collateralVaultAuthority,
           depositHistory: clearingHouseState.depositHistory,
-          fundingPaymentHistory: clearingHouseState.fundingPaymentHistory,
+          insuranceVault: clearingHouseState.insuranceVault,
+          insuranceVaultAuthority: clearingHouseState.insuranceVaultAuthority,
           tokenProgram: token.TOKEN_PROGRAM_ID,
-        }
-      });
-
-      let userAccount = await CH_program.account.user.fetch(userAccountPublicKey);
-      // console.log(userAccount)
-      assert(userAccount.collateral.eq(depositAmount))
-
-      // view current market conditions 
-      const solUsd = clearingHouse.getMarket(marketIndex).amm.oracle;
-      const pythClient = new drift.PythClient(connection)
-      var market = clearingHouse.getMarket(marketIndex);
-      // get oracle/mark price 
-      var solUsdcData = await getFeedData(pyth_program, solUsd)
-      var currentMarketPrice = drift.calculateMarkPrice(market);
-      console.log("sol usdc price (mark):", currentMarketPrice.toString()) 
-      console.log("sol usdc price (oracle):", solUsdcData.price) 
-      // compute funding rate
-      var estimated_funding = await drift.calculateEstimatedFundingRate(
-        market, 
-        await pythClient.getOraclePriceData(solUsd),
-        new BN(1), 
-        "interpolated"
-      );
-      console.log("estimated funding:", estimated_funding.toString());
-
-      // go long on the mock market (USDC/SOL)
-      console.log("going long...")
-
-      let quote_amount_in = depositAmount;
-      let limitPrice = new BN(0); // yolo
-      const optionalAccounts_position = {
-        discountToken: false,
-        referrer: false,
-      };
-
-      await CH_program.rpc.openPosition(
-        drift.PositionDirection.LONG,
-        quote_amount_in, 
-        marketIndex, // only market = 0 
-        limitPrice,
-        optionalAccounts_position, 
-        {
-          accounts: {
-            // user stuff 
-            state: ch_state_pk,
-            user: userAccountPublicKey, 
-            userPositions: userPositions.publicKey,
-            authority: provider.wallet.publicKey,
-            // market oracle PK 
-            oracle: solUsd,
-            // CH stuff 
+          updatePosition: {
+            authority: authority, 
+            userPositions: user_positions,
+            state: clearingHouseStatePk,
+            user: user_account,
             markets: clearingHouseState.markets,
             tradeHistory: clearingHouseState.tradeHistory,
             fundingPaymentHistory: clearingHouseState.fundingPaymentHistory,
-            fundingRateHistory:clearingHouseState.fundingRateHistory,
-          }
+            fundingRateHistory: clearingHouseState.fundingRateHistory,
+            oracle: solUsd,
+            clearingHouseProgram: CH_program.programId,
+          },
         }
-      );
+      }
+    );
+    
+    let tx = new web3.Transaction().add(ix);
 
-      let positions = await CH_program.account.userPositions.fetch(
-        userAccount.positions as web3.PublicKey
-      );
-      let position = positions.positions[0];
-      assert(depositAmount.eq(position.quoteAssetAmount));
+    // let resp = await provider.simulate(tx);
+    // console.log(resp)
 
-      // re-compute funding rate (should change bc of mark/oracle difference)
-      var market = clearingHouse.getMarket(marketIndex); // update market data post trade 
+    await provider.send(tx);
 
-      // await setFeedPrice(pyth_program, solUsdcData.price * 1.02, solUsd) // update oracle UP
-      // await setFeedPrice(pyth_program, solUsdcData.price * 0.95, solUsd) // update oracle DOWN
+    var user_usdc_balance_end = await get_token_balance(userUSDCAccount.publicKey)
+    assert(user_usdc_balance_end.gt(user_usdc_balance)) // got more USDC
 
-      // get oracle/mark price 
-      var solUsdcData = await getFeedData(pyth_program, solUsd)
-      var currentMarketPrice = drift.calculateMarkPrice(market); // AMM xy = k 
-      console.log("sol usdc price (mark):", currentMarketPrice.toString()) 
-      console.log("sol usdc price (oracle):", solUsdcData.price) 
-      
-      // compute funding rate 
-      var estimated_funding = await drift.calculateEstimatedFundingRate(
-          market, 
-          await pythClient.getOraclePriceData(solUsd), 
-          new BN(1), 
-          "interpolated"
-      );
-      console.log("estimated funding:", estimated_funding.toString());
+    var user_vault_balance_end = await get_token_balance(user_vault_ata)
+    assert(user_vault_balance_end.eq(user_vault_balance.sub(burn_amount))); // less vault tokens 
 
-  });
-
+  })
 });
